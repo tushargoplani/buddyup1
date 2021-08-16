045d86172926443eef8fc750e29b93626a2795a3
tushargoplani <tushar7140@gmail.com>
2021-08-16 19:11:00 +0530

    UI updated,set profile pic and user can update their details

---
+++ buddyupbackend/multerConfig.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var multer  =  require('multer');
+
+
+var storage = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+                        cb(null, 'userImages')
+                    },
+        filename: function (req, file, cb) {
+                    var ext = path.extname(file.originalname);
+                    console.log("in config 12---trying to upload --"+file.fieldname.slice(0,3) + '-' + Date.now()+"."+ext)
+                    cb(null, file.fieldname.slice(0,3) + '-' + Date.now()+"."+ext);
+                }
+  }
+
+//   resume-234763458969876.jpg
+  
+  )
+
+  
+  var multerOptions = { storage: storage,
+                        fileFilter: function(req,file,callback){
+                                        var ext = path.extname(file.originalname);
+                                        var fieldName = file.fieldname;
+
+                                        if(fieldName=="profile")
+                                        {
+                                            if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+                                                return callback(new Error('Only images are allowed for profile'));
+                                            }
+                                            callback(null, true);
+                                        }
+                                      
+                                        
+                                    } 
+                      }
+   
+  var upload = multer(multerOptions).fields([{name:"profile", maxCount:1}]);
+
+
+  module.exports=upload;
